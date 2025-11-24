6c3bcf051e10030528571a6e06fa60c77860c431
jun <jun.drums.1102@gmail.com>
2025-11-24 15:36:56 +1100

    modify google adsense script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-import Script from 'next/script';
 import type { Metadata } from 'next';
 import './globals.css';
 
@@ -18,16 +17,13 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ja">
-      <head>
-        <Script
-          id="adsbygoogle-loader"
-          strategy="afterInteractive"
-          async
-          crossOrigin="anonymous"
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5434162081070782"
-        />
-      </head>
+      <head />
       <body>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5434162081070782"
+          crossOrigin="anonymous"
+        />
         <main>{children}</main>
       </body>
     </html>
